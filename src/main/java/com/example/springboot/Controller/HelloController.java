e0556861155001b88665400e41a6b8d7567c07eb
Walter Diaz <wdiaz@cs.columbia.edu>
2020-10-13 00:22:23 -0400

    added find all method

--- src/main/java/com/example/springboot/Controller/HelloController.java
+++ src/main/java/com/example/springboot/Controller/HelloController.java
@@ -1,14 +1,26 @@
 package com.example.springboot.Controller;
 
+import com.example.springboot.Entity.Mentions;
+import com.example.springboot.Repository.MentionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-
+	@Autowired
+	private MentionsRepository mentionsRepository;
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping(path="/all")
+	public @ResponseBody
+	Iterable<Mentions> getAllMentions() {
+		return mentionsRepository.findAll();
 	}
 
 }
