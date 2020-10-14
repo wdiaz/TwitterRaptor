ac06e379587f5bb1779a8abcbba9bfa3a776fc9c
Walter Diaz <wdiaz@cs.columbia.edu>
2020-10-14 10:16:45 -0400

    added basic selenium logic

--- src/main/java/com/example/springboot/Controller/HelloController.java
+++ src/main/java/com/example/springboot/Controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.springboot.Controller;
 
+import com.example.springboot.Application;
 import com.example.springboot.Entity.Mentions;
 import com.example.springboot.Repository.MentionsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +18,9 @@ import java.util.List;
 public class HelloController {
 	@Autowired
 	private MentionsRepository mentionsRepository;
+
+	private static final Logger logger= LoggerFactory.getLogger(Application.class);
+
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
@@ -42,6 +48,13 @@ public class HelloController {
 	public String mentions() throws TwitterException {
 		Twitter twitter = TwitterFactory.getSingleton();
 		List<Status> statuses = twitter.getMentionsTimeline();
-		return statuses.toString();
+		for(Status status : statuses) {
+			String inReplyToScreenNAme = status.getInReplyToScreenName();
+			String inReplyToStatusId = String.valueOf(status.getInReplyToStatusId());
+			String tweetText = status.getText();
+			logger.info(inReplyToStatusId + " = " + inReplyToScreenNAme + "=" + tweetText);
+		}
+		//return statuses.toString();
+		return "ok";
 	}
 }
--- src/main/java/com/example/springboot/Controller/SeleniumController.java
+++ src/main/java/com/example/springboot/Controller/SeleniumController.java
@@ -1,2 +1,25 @@
-package com.example.springboot.Controller;public class SeleniumController {
+package com.example.springboot.Controller;
+
+import com.example.springboot.Service.WebsiteCrawler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class SeleniumController {
+
+    @GetMapping("/capture")
+    public String capture() {
+        return "ok";
+    }
+
+    @RequestMapping("/screenshots")
+    public String index(@RequestParam("url") String url) {
+        WebsiteCrawler websiteCrawler = new WebsiteCrawler();
+        websiteCrawler.crawl(url);
+        return url;
+    }
 }
