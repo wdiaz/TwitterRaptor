54f5c10adfbcb29a4d704ae524d4b8a280769d60
Walter Diaz <wdiaz@cs.columbia.edu>
2020-11-19 00:33:07 -0500

    stuff

--- src/main/java/com/example/springboot/Controller/SeleniumController.java
+++ src/main/java/com/example/springboot/Controller/SeleniumController.java
@@ -1,20 +1,20 @@
 package com.example.springboot.Controller;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.slf4j.Logger;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
-import java.awt.event.KeyEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
@@ -78,16 +78,50 @@ public class SeleniumController {
 
     @RequestMapping("ff")
     public String ff(@RequestParam("url") String url) throws IOException {
+        logger = LoggerFactory.getLogger(SeleniumController.class);
+
+
+
+
         System.setProperty("webdriver.gecko.driver", "/Users/wdiaz/Downloads/geckodriver");
-        WebDriver driver = new FirefoxDriver();
+
+        /*String userAgent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16";
+        FirefoxProfile firefoxProfile = new FirefoxProfile();
+        firefoxProfile.setPreference("general.useragent.override", userAgent);*/
+
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        //firefoxOptions.setProfile(firefoxProfile);
+
+        //firefoxOptions.setHeadless(true);
+
+        WebDriver driver = new FirefoxDriver(firefoxOptions);
+
+
+        Dimension size = driver.manage().window().getSize();
+        logger.info("Window Height: " + size.getHeight());
+        logger.info("Window Width: " + size.getWidth());
+
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         //driver.get("about:reader?url=" + url);
         driver.get(url);
 
+
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        boolean isScrollBarPresent = (boolean) js.executeScript("return document.documentElement.scrollHeight>document.documentElement.clientHeight");
+        long scrollHeight = (long) js.executeScript("return document.documentElement.scrollHeight");
+        long clientHeight = (long) js.executeScript("return document.documentElement.clientHeight");
+
+        logger.info("isScrollBarPresent: " + isScrollBarPresent);
+        logger.info("scrollHeight: " + scrollHeight);
+        logger.info("clientHeight" + clientHeight);
+
+       double totalHeight = scrollHeight - (scrollHeight * 0.40);
+
+        //js.executeScript("window.scrollTo(0, " +totalHeight + ")");
+        js.executeScript("window.scrollTo(0, document.documentElement.scrollHeight)");
+        //js.executeScript("window.scrollTo(0, 500)");
 
 
         try {
