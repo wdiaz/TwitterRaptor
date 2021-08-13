9b4094c607798149015d017febb3f109eea6e800
Walter Diaz <wdiaz@cs.columbia.edu>
2021-08-13 02:15:46 -0400

    updated repository file

--- src/main/java/com/example/springboot/Repository/MentionRepository.java
+++ src/main/java/com/example/springboot/Repository/MentionRepository.java
@@ -2,7 +2,9 @@ package com.example.springboot.Repository;
 
 import com.example.springboot.Entity.Mention;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MentionRepository extends JpaRepository<Mention, Integer> {
     boolean existsByRequestorTweetIdAndTargetTweetId(String requestorTweetId, String targetTweetId);
 }
